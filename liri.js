bfe8ffb1954168191d1fee90ea0c00a151542bd4
ShaToyaJones <jshatoyaj@yahoo.com>
2017-10-10 11:58:38 -0400

    minor changes

--- liri.js
+++ liri.js
@@ -4,18 +4,36 @@ var fs = require("fs");
 fs.writeFile("random.txt", "")
 
 //Store the textfile filename given to us from the command line.
+var twitter = require("twitter");
+var spotify = require("spotify");
+var inquirer = require("inquirer");
+var keys = require("./keys");
+
 var textFile = process.argv[2];
 
-//Append the contents into the file.
-fs.appendFile(randomtxt, "", function(err) {
-	
-	//If an error is experienced we display it here:
-	if (err) {
-		console.log(err);
-	}
+//Twitter username inside of a parameter object.
+var params = {
+	screen_name: "stjones1920"
+} && {
+	count: 20
+};
 
-	//If no error is experienced we'll log this phrase "Content Added".
-	else {
-		console.log("Content Added!");
-	}
-});
+
+//A function to grab the tweets from Twitter.
+function getTwitter() {
+
+}
+
+// //Append the contents into the file.
+// fs.appendFile(randomtxt, "", function(err) {
+	
+// 	//If an error is experienced we display it here:
+// 	if (err) {
+// 		console.log(err);
+// 	}
+
+// 	//If no error is experienced we'll log this phrase "Content Added".
+// 	else {
+// 		console.log("Content Added!");
+// 	}
+// });
