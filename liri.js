e6fa8848871adb4030b988c3b11db8648a74e8a7
ShaToyaJones <jshatoyaj@yahoo.com>
2017-10-13 18:17:38 -0400

    no longer throwing the client.get error for Twitter

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 fs.writeFile("random.txt", "")
 
 //The code needed to grab the data from keys.js for #7
-var keys = require("./keys");
+var keys = require("./keys.js");
 
 //Store the textfile filename given to us from the command line.
 var twitter = require("twitter");
@@ -12,24 +12,46 @@ var spotify = require("spotify");
 var inquirer = require("inquirer");
 var liriCommand = process.argv[2];
 
-switch (twitter) {
-	case "tweet":
-		tweet();
-		break;
+switch (liriCommand) {
+	case "my-tweets": 
+	getTwitter();
+	break;
+
+	case "spotify-this-song":
+	getSpotify();
+	break;
+
+	case "movie-this":
+	getMovieInfo();
+	break;
+
+	case "do-what-it-says":
+	random();
+	break;
 }
-
-//Twitter username inside of a parameter object.
-var params = {
-	screen_name: "stjones1920"
-} && {
-	count: 20
-};
-
 
 //A function to grab the tweets from Twitter.
 function getTwitter() {
 
-}
+var client = new Twitter ({
+	consumer_key: keys.twitterKeys.consumer_key,
+	consumer_secret: keys.twitterKeys.consumer_secret,
+	access_token_key: keys.twitterKeys.access_token_key,
+	access_token_secret: keys.twitterKeys.access_token_secret
+});
+
+//Twitter username inside of a parameter object.
+var params = {
+	screen_name: "stjones1920",
+	count: 20};
+	client.get("statuses/user_timeline", params, function(error, tweets, response) {
+		if (!error) {
+			console.log(tweets);
+		}
+// } && {
+// 	count: 20
+	});
+};
 
 // //Append the contents into the file.
 // fs.appendFile(randomtxt, "", function(err) {
